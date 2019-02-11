df21cc7172d2afa5645e64a8c14a56ec4e547d67
U.A. Yanık <ates.yanik@hva.nl>
2019-02-12 00:24:06 +0100

    second  commit

--- app/src/main/java/com/example/a01_picturequiz/MainActivity.java
+++ app/src/main/java/com/example/a01_picturequiz/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,33 +21,44 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup mGroup;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mImageView = findViewById(R.id.imageView);
-        mCheckButton = findViewById(R.id.button);
+        mCheckButton = findViewById(R.id.checkButton);
         mGroup = findViewById(R.id.radioGroup);
 
-        mImageNames = new int[] {R.drawable.image1, R.drawable.image2, R.drawable.image3}
+        mImageNames = new int[]{R.drawable.image1, R.drawable.image2, R.drawable.image3};
 
 
         mCheckButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
-
             public void onClick(View v) {
-
                 int radioButtonID = mGroup.getCheckedRadioButtonId();
-
                 View radioButton = mGroup.findViewById(radioButtonID);
-
                 int answerIndex = mGroup.indexOfChild(radioButton);
+                checkAnswer (answerIndex);
+            }
 
-                checkAnswer(answerIndex);
+            private void checkAnswer(int answer) {
+                String message;
+                if (answer == currentImageIndex) {
+                    message = "Great";
+                    nextPicture();
+                } else {
+                    message = "Wrong";
+                }
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
 
+            private void nextPicture(){
+                currentImageIndex++;
+                if (currentImageIndex >= mImageNames.length) {
+                    currentImageIndex = 0;
+                }
+                mImageView.setImageResource(mImageNames[currentImageIndex]);
             }
 
         });
