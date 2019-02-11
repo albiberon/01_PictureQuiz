846d11dc1c497461576fd5349c734a94c58fed82
U.A. Yanık <ates.yanik@hva.nl>
2019-02-11 23:54:16 +0100

    first commit

---
+++ app/src/main/java/com/example/a01_picturequiz/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.a01_picturequiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int currentImageIndex = 0;
+
+    private int[] mImageNames;
+
+    private ImageView mImageView;
+
+    private Button mCheckButton;
+
+    private RadioGroup mGroup;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.imageView);
+        mCheckButton = findViewById(R.id.button);
+        mGroup = findViewById(R.id.radioGroup);
+
+        mImageNames = new int[] {R.drawable.image1, R.drawable.image2, R.drawable.image3}
+
+
+        mCheckButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                int radioButtonID = mGroup.getCheckedRadioButtonId();
+
+                View radioButton = mGroup.findViewById(radioButtonID);
+
+                int answerIndex = mGroup.indexOfChild(radioButton);
+
+                checkAnswer(answerIndex);
+
+            }
+
+        });
+
+    }
+
+
+}
